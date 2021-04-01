d1d6cbf65c6ca0f14587fae01c32ebd40b1cd5e5
ZolF-werk <zolf@bk.ru>
2021-04-01 17:07:47 +0300

    Last_stand

--- dist/JS/script.js
+++ dist/JS/script.js
@@ -10,3 +10,10 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 //JS to js ***
+
+//**********__%__Percents*********/
+const percents = document.querySelectorAll('.skills__scale-percent'),
+    bars = document.querySelectorAll('.skills__scale-bar span');
+percents.forEach((item, i) => {
+    bars[i].style.width = item.innerHTML;
+});
